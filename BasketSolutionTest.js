f500d1a8fb0db7837cfdb2ac174fc4b474b00012
kyle <kylebellle7@gmail.com>
2019-08-12 09:20:36 -0400

    initial test

---
+++ BasketSolutionTest.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+
+describe("BasketSolution", () => {
+    it("Should exist", () => {
+        let BasketSolution = require("./BasketSolution");
+        expect(BasketSolution).to.not.be.undefined
+    });
+})
---
+++ README.md
@@ -0,0 +1,1 @@
+#Basket Solution For GroupXS
