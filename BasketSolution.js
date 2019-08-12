56cad92509a8cdc32448e2b06ef160b64189226b
kyle <kylebellle7@gmail.com>
2019-08-12 15:52:42 -0400

    Added tests and methods for calculating gross total and discount

--- BasketSolution.js
+++ BasketSolution.js
@@ -1,4 +1,5 @@
 var basketSolution = {
+
     basketCount: (basket = null) => {
         if(basket){
             let count = 0;
@@ -12,6 +13,7 @@ var basketSolution = {
             return null;
         }
     },
+
     validateBasket: (basket = null) => {
         if(basket){
             if(basket.id.indexOf(" ") === -1){
@@ -22,7 +24,142 @@ var basketSolution = {
         }else{
             return null;
         }
+    },
+
+    calcGrossTotal: (basket = null) => {
+        if(basket){
+            let total = 0;
+            basket.items.forEach((item) => {
+                total += item.price * item.amount;
+            });
+
+            return total;
+        }else{
+            return null;
+        }
+
+    },
+
+    calcDiscount: async (basket = null) => {
+        if(basket){
+            let discountTotal = 0;
+
+            /* This approach does not work for reason i do not yet know */
+            // await discount_offers.forEach(async (discount) => {
+            //     let discountAmount = await discount.fn(basket);
+
+            //     console.log("discount amount", discountAmount);
+
+            //     if(discountAmount > 0){
+            //         discountTotal += discountAmount;
+            //         console.log("discountTotal in fn scope", discountTotal);
+            //         console.log(`${discount.discount} Discount : $${discountAmount}`);
+            //     }
+            // });
+
+            for(let i = 0; i <= discount_offers.length - 1; i++){
+                let discountAmount = await discount_offers[i].fn(basket);
+
+                if(discountAmount > 0){
+                    discountTotal += discountAmount;
+                    //console.log per purposely left here
+                    console.log(`${discount_offers[i].discount} Discount : $${discountAmount}`);
+                }
+            }
+
+            return discountTotal;
+        }else{
+            return null;
+        }
     }
 };
+
+async function calcGrossTotalForDiscount(items){
+    if(items){
+        let total = 0;
+        await items.forEach((item) => {
+            total += item.price * item.amount;
+        });
+
+        return total;
+    }else{
+        return null;
+    }
+};
+
+// I suggest we have an array of discount funtions which we run the baskets through
+var discount_offers = [
+    {discount: "Potter_mania", fn: async (basket /*should always be passed*/) => {
+
+        let distinct_books = [];
+
+        // because only one instance of an object is store for a particular item_name there should not be duplicates
+
+        distinct_books = await basket.items.filter((item) => {
+            //preferrable this would be ddone by id or something more uniquely identifiable 
+            switch(item.item_name){
+                case "First book":
+                    return true;
+
+                case "Second book":
+                    return true;
+
+                case "Third book":
+                    return true;
+
+                case "Fourth book":
+                    return true;
+
+                case "Fifth book":
+                    return true;
+
+                default:
+                    return false;
+            }
+        });
+
+        //In the current state length will not be more than five
+        let discount_value;
+        switch(distinct_books.length){
+            case 0:
+                discount_value = 0;
+            break;
+
+            case 1:
+                discount_value = 0;
+            break;
+
+            case 2:
+                //console.log per purposely left here
+                console.log("Potter_mania discount 2");
+                discount_value = 0.05 * await calcGrossTotalForDiscount(distinct_books);
+                //return discount_value;
+            break;
+
+            case 3:
+                //console.log per purposely left here
+                console.log("Potter_mania discount 3");  
+                discount_value = 0.1 * await calcGrossTotalForDiscount(distinct_books);
+                //return discount_value;
+            break;
+
+            case 4:
+                //console.log per purposely left here
+                console.log("Potter_mania discount 4");                
+                discount_value = 0.2 * await calcGrossTotalForDiscount(distinct_books);
+                //return discount_value;
+            break;
+
+            case 5:
+                //console.log per purposely left here
+                console.log("Potter_mania discount 5");   
+                discount_value = 0.25 * await calcGrossTotalForDiscount(distinct_books);
+                //return discount_value;
+            break;
+        }
+
+        return discount_value;
+    }}
+]
 
 module.exports = basketSolution;
--- BasketSolutionTest.js
+++ BasketSolutionTest.js
@@ -16,8 +16,8 @@ describe("basketCount()", () => {
         let input = {
             basket_id: "1",
             items: [
-                {item_name: "First book", amount: 1},
-                {item_name: "Third book", amount: 3}
+                {item_name: "First book", amount: 1, price: 8.00},
+                {item_name: "Third book", amount: 3, price: 8.00}
             ]
             //...
         }
@@ -31,9 +31,9 @@ describe("basketCount()", () => {
         input = {
             basket_id: "2",
             items: [
-                {item_name: "First book", amount: 2},
-                {item_name: "Second book", amount: 3},
-                {item_name: "Fourth book", amount: 3}
+                {item_name: "First book", amount: 2, price: 8.00},
+                {item_name: "Second book", amount: 3, price: 8.00},
+                {item_name: "Fourth book", amount: 3, price: 8.00}
             ]
             //...
         }
@@ -68,6 +68,88 @@ describe("validateBasket()", () => {
 
         expect(valid).to.equal(expected);
     });
+});
+
+describe("calcGrossTotal()", () => {
+    it("should calculate the gross total for a given basket, that is the total before any discounts or fee are added/deducted.", () => {
+        let expected = 24;
+
+        let input = {
+            id: "123",
+            items: [
+                {item_name: "First book", amount: 2, price: 8.00},
+                {item_name: "Fifth book", amount: 1, price: 8.00}
+            ]
+        };
+
+        let total = BasketSolution.calcGrossTotal(input);
+
+        expect(total).to.equal(expected);
+
+        expected = 16;
+
+        input = {
+            id: "123",
+            items: [
+                {item_name: "Second book", amount: 1, price: 8.00},
+                {item_name: "Fifth book", amount: 1, price: 8.00}
+            ]
+        };
+
+        total = BasketSolution.calcGrossTotal(input);
+
+        expect(total).to.equal(expected);
+    });
+})
+
+describe("calcDiscount()", async () => {
+    await it("Should calculate the discount for a given basket.", async () => {
+        let expected = 0;
+
+        let input = {
+            id: "321",
+            items: [
+                {item_name: "First book", amount:3, price: 8.00}
+            ]
+        }
+
+        let discount = await BasketSolution.calcDiscount(input);
+
+        expect(discount).to.equal(expected);
+
+        expected = 16 * 0.05;
+
+         input = {
+            id: "321",
+            items: [
+                {item_name: "First book", amount: 1, price: 8.00},
+                {item_name: "Second book", amount: 1, price: 8.00}
+            ]
+        }
+
+        discount = await BasketSolution.calcDiscount(input);
+
+        expect(discount).to.equal(expected);
+
+        expected = 12.8;
+
+         input = {
+            id: "321",
+            items: [
+                // book name should be stored somewhere and retrieved from elsewhere just too lazy right now
+                {item_name: "First book", amount: 2, price: 8.00},
+                {item_name: "Second book", amount: 2, price: 8.00},
+                {item_name: "Third book", amount: 2, price: 8.00},
+                {item_name: "Fourth book", amount: 1, price: 8.00},
+                {item_name: "Fifth book", amount: 1, price: 8.00}
+            ]
+        }
+
+        discount = await BasketSolution.calcDiscount(input);
+
+        expect(discount).to.equal(expected);
+    });
 
     
 });
+
